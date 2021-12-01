33c5b58b90032195cdc1e8549c293b6b1ffa5fbf
Theres Wallinder <52582658+Nexya@users.noreply.github.com>
2021-12-01 09:32:27 +0100

    day 1 part 2 done

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -6,10 +6,22 @@ def getInput():
 
 
 sonar = getInput()
-increases = 0
+
+#Part 1
+increases1 = 0
 
 for i in range (1,len(sonar)):
     if sonar[i] > sonar[i-1]:
-        increases += 1
+        increases1 += 1
 
-print("Total sonar increases: ", increases)
+#Part 2
+increases2 = 0
+
+for i in range(3,len(sonar)):
+    sum1 = sonar[i-3] + sonar[i-2] + sonar[i-1]
+    sum2 = sonar[i-2] + sonar[i-1] + sonar[i]
+    if sum2 > sum1:
+        increases2 += 1
+
+print("Total sonar increases part 1: ", increases1)
+print("Total sonar increases part 2: ", increases2)
