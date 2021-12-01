4e1fcdf7f32f26bd588889aaa31937e684310a76
Theres Wallinder <52582658+Nexya@users.noreply.github.com>
2021-12-01 09:17:36 +0100

    day 1 solution

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,5 +1,15 @@
 def getInput():
-    with open("input.txt") as file:
+    with open("2021\day1\input.txt") as file:
         lines = file.readlines()
     numbers = [int(line.strip()) for line in lines]
     return numbers
+
+
+sonar = getInput()
+increases = 0
+
+for i in range (1,len(sonar)):
+    if sonar[i] > sonar[i-1]:
+        increases += 1
+
+print("Total sonar increases: ", increases)
