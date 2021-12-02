2d8f4351a4f735ca4cb3de9864482b1a40cb554c
Theres Wallinder <52582658+Nexya@users.noreply.github.com>
2021-12-02 18:25:40 +0100

    day 2 done

--- 2021/day2/day2.java
+++ 2021/day2/day2.java
@@ -3,43 +3,48 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class day2{
+
+    public static int aim = 0;
+    public static int depth = 0;
+    public static int horizontal= 0;
+
     public static void main(String[] args) throws FileNotFoundException {
-        int depth = 0;
-        int horizontal = 0;
 
         File file = new File("2021\\day2\\input.txt");
         Scanner scanner = new Scanner(file);  
         
         while(scanner.hasNextLine()){
             String number = scanner.nextLine();
-            depth += calcDepth(number);
-            horizontal += calcHorizontal(number);
+            
+            if(!number.contains("forward")){
+                calcAim(number);
+            } else{
+                calcHorizontal(number);
+            }
+            
         }
 
         System.out.println("Total depth: " + depth);
         System.out.println("Total movement forward: " + horizontal);
         System.out.println("Muliplied: " + depth*horizontal);
+        scanner.close();
     }
 
-    public static int calcDepth(String move){
-        int depth = 0;
+    public static void calcAim(String move){
         int number = Integer.parseInt(move.substring(move.length()-1).trim());
 
         if(move.contains("down")){
-            depth += number;
+            aim += number;
         } else if(move.contains("up")){
-            depth -= number;
+            aim -= number;
         }
-        return depth;
     }
 
-    public static int calcHorizontal(String move){
-        int horizontal = 0;
+    public static void calcHorizontal(String move){
         int number = Integer.parseInt(move.substring(move.length()-1).trim());
 
-        if(move.contains("forward")){
-            horizontal += number;
-        }
-        return horizontal;
+        horizontal += number;
+        depth += (aim*number);
+        
     }
 }
